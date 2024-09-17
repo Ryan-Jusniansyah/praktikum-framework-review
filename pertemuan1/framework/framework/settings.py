f24bd7a8f9581977048d917d0087a44c24350c7d
J_Ryan <114414641+Ryan-Jusniansyah@users.noreply.github.com>
2024-09-17 12:05:02 +0800

    Review2

--- pertemuan1/framework/framework/settings.py
+++ pertemuan1/framework/framework/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ais", #Untuk mendaftaran aplikasi
     "django_bootstrap5",
+    "django_seed"
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "framework.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'ais',
+        "USER": 'root',
+        "PASSWORD": '',
+        "HOST": '127.0.0.1',
+        "PORT": '3306',
     }
 }
 
