d453aa8b17f9905908fcd23cce53275e6fd44643
J_Ryan <114414641+Ryan-Jusniansyah@users.noreply.github.com>
2024-10-08 15:04:01 +0800

    Review 5

--- pertemuan1/framework/ais/urls.py
+++ pertemuan1/framework/ais/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("", views.homepage),
     path("student/", views.student_index, name="student_index"),
-    path("student/create/", views.student_create, name="student_create")
+    path("student/create/", views.student_create, name="student_create"),
+    path("student/update/<int:student_id>/", views.student_update, name='student_update'),
+    path('student/delete/<int:student_id>', views.student_delete, name='student_delete'),
 ]
